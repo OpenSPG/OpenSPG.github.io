02bb5a2311f6c72af97aeced2103baaffab9d7df
Aaron <yangtao.yangtao@antgroup.com>
2023-10-26 13:21:07 +0800

    feat: 添加兼容性检测机制 (#6)
    * chore: enable compaticity config
    
    * feat: add compacity detect

--- .dumi/theme/layouts/GlobalLayout.tsx
+++ .dumi/theme/layouts/GlobalLayout.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import { ConfigProvider, Result, theme, Modal } from 'antd';
 import { useOutlet, usePrefersColor } from 'dumi';
-import { ConfigProvider, theme } from 'antd';
+import React, { useEffect } from 'react';
+import { isMobile } from 'react-device-detect';
 
 const GlobalLayout: React.FC = () => {
   const outlet = useOutlet();
   const [color] = usePrefersColor();
+  const [isBtnVisible, setIsBtnVisible] = React.useState(true);
+  const isCompact = isMobile && isBtnVisible;
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsBtnVisible(true);
+          } else {
+            setIsBtnVisible(false);
+          }
+        });
+      },
+      {
+        threshold: 0.5,
+      },
+    );
+
+    const element = document.getElementsByClassName(
+      'dumi-default-header-menu-btn',
+    )?.[0];
+    if (element) {
+      observer.observe(element);
+    }
+
+    return () => {
+      if (element) {
+        observer.unobserve(element);
+      }
+    };
+  }, []);
 
   return (
     <ConfigProvider
@@ -14,6 +47,18 @@ const GlobalLayout: React.FC = () => {
           color === 'dark' ? theme.darkAlgorithm : theme.defaultAlgorithm,
       }}
     >
+      <Modal open={!isCompact}>
+        <Result
+          status="error"
+          title={'不兼容的浏览器\nUnsupported browser!'}
+          subTitle={
+            <>
+              <div>请更换浏览器访问</div>
+              <div>Please change your browser</div>
+            </>
+          }
+        />
+      </Modal>
       {outlet}
     </ConfigProvider>
   );
