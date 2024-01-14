42d05727cf33dadf7e8940d72632490ef862a3a8
Aarebecca <Aarebecca@users.noreply.github.com>
2024-01-14 11:46:16 +0000

    deploy: 61947caf8ec3e39a5eb482449c2ab4cdb8ad0f8b

---
+++ dumi__pages__404.33ef1b12.async.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkspg_site=self.webpackChunkspg_site||[]).push([[7400],{88631:function(a,n,t){t.r(n);var s=t(87363),e=t.n(s),u=t(86074);n.default=function(){return(0,s.useEffect)(function(){location.href=location.origin},[]),(0,u.jsx)(u.Fragment,{})}}}]);
