107d5956f73b672b333273cc1cb0f2fb70daf2f4
echoOikawa <103234654+echoOikawa@users.noreply.github.com>
2025-01-08 17:10:18 +0800

    style: update help doc (#21) (#22)
    Co-authored-by: mutu <gegenhasi.gghs@antgroup.com>

--- .dumi/pages/guide.tsx
+++ .dumi/pages/guide.tsx
@@ -15,11 +15,12 @@ export default () => {
 
   return <div>
     <Title level={4}>{intl('版本', 'Version')}</Title>
-    <Redirect url={intl('https://openspg.yuque.com/ndx6g9/0.5', 'https://openspg.yuque.com/ndx6g9/wc9oyq')} target='_blank' text=" " />
+    <Redirect url={intl('https://openspg.yuque.com/ndx6g9/docs', 'https://openspg.yuque.com/ndx6g9/docs_en')} target='_blank' text=" " />
     <Flex vertical align={'flex-start'}>
       <Button type="link" target="_blank" href={intl('https://openspg.yuque.com/ndx6g9/ooil9x', 'https://openspg.yuque.com/ndx6g9/ns5nw2')}>0.0.2</Button>
       <Button type="link" target="_blank" href={intl('https://openspg.yuque.com/ndx6g9/nmwkzz', 'https://openspg.yuque.com/ndx6g9/wc9oyq')}>0.0.3</Button>
       <Button type="link" target="_blank" href={intl('https://openspg.yuque.com/ndx6g9/0.5', 'https://openspg.yuque.com/ndx6g9/wc9oyq')}>0.5</Button>
+      <Button type="link" target="_blank" href={intl('https://openspg.yuque.com/ndx6g9/docs', 'https://openspg.yuque.com/ndx6g9/docs_en')}>0.6</Button>
     </Flex>
   </div>;
 };
